66769da69f94f0ef5ce0f9569491419f2f3331e6
andoni10 <videojuegos.eui-vi@hotmail.com>
2019-02-07 13:31:52 +0100

    createtable

---
+++ MiniSQLEngine/CreateTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for CreateTable
+/// </summary>
+public class CreateTable
+{
+	public CreateTable()
+	{
+        string regExp = @"CREATE\s+TABLE\s+(\w+)\s+\(([^()]*)\);";
+    }
+}
