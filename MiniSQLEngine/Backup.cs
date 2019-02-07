f78c8deb037f1f7f51a07b8835bb6163c80d8e7b
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-07 13:32:23 +0100

    update

---
+++ MiniSQLEngine/Backup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MiniSQLEngine
+{
+    class Backup
+    {
+        public static void backupCheck(string input)
+        {
+            string regExp = @"BACKUP DATABASE\s(\w+) TO DISK = ('\w+');";
+            foreach (Match m in Regex.Matches(input, regExp))
+            {
+                Console.WriteLine("Groups[0]= " + m.Groups[0].Value + "\n");
+                Console.WriteLine("Groups[1]= " + m.Groups[1].Value + "\n");
+                Console.WriteLine("Groups[2]= " + m.Groups[2].Value + "\n");
+            }
+        }
+}
+}
