856c32a543cf90ec58478448a0b9e6ba338e0f0c
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-07 12:32:18 +0100

    Initial

---
+++ MiniSQLEngine/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine
+{
+    public class Engine
+    {
+        public bool Query(string queryString, out string result)
+        {
+            result = Constants.NotImplementedError;
+            return true;
+        }
+    }
+}
---
+++ Test/Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniSQLEngine;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MiniSQLParse1()
+        {
+            string result;
+            Engine engine = new Engine();
+            engine.query("Select * from Table1", out result);
+            Assert.AreEqual(Constants.NotImplementedError, result);
+        }
+    }
+}
