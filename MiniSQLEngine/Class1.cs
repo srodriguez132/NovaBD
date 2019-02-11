6cab5f16d5a92cecbfce2276b8e933a194a716eb
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-11 13:25:49 +0100

    Select and delete

---
+++ MiniSQLEngine/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Select
+{
+	public Select()
+	{
+        SELECT\s + (\w +|\*)\s + FROM\s + (\w +)\s + WHERE\s + (\w +)= (\w +);
+    }
+}
