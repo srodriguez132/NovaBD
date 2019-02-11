50f489bcb0086f0aa07effb84354413bbc3da695
andoni10 <videojuegos.eui-vi@hotmail.com>
2019-02-11 11:48:19 +0100

    createTable

--- MiniSQLEngine/CreateTable.cs
+++ MiniSQLEngine/CreateTable.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 /// <summary>
 /// Summary description for CreateTable
@@ -8,5 +13,11 @@ public class CreateTable
 	public CreateTable()
 	{
         string regExp = @"CREATE\s+TABLE\s+(\w+)\s+\(([^()]*)\);";
+        string input = "CREATE TABLE tabla (name string, edad int)";
+        Match match = regExp.Match(regExp, input);
+        if (match.Success)
+        {
+
+        }
     }
 }
