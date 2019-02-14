159aae21addd1112cec96d6b9948bd6af06854db
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-14 12:34:56 +0100

    SelectTest and DeleteTest

--- MiniSQLEngine/Select.cs
+++ MiniSQLEngine/Select.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +11,15 @@ namespace MiniSQLEngine
     {
         public Match delete(string query)
         {
-            string regExp = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+)=(\w+);";
+            string regExp = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+)\s+=\s+(\w+);";
             Match match = Regex.Match(query, regExp);
             return match;
+            
         }
 
         public Match select(string query)
         {
-            string regExp = @"SELECT\s+(\w+|\*)(\s+|\,\s+\w+\s+)FROM\s+(\w+)(\s+WHERE\s+(\w+)\s+=\s+(\w+);|;)";
+            string regExp = @"SELECT\s+(\w+|\*)(\s+|\,\s+(\w+)\s+)FROM\s+(\w+)(\s+WHERE\s+(\w+)\s+=\s+(\w+);|;)";
             Match match = Regex.Match(query, regExp);
             return match;
         }
