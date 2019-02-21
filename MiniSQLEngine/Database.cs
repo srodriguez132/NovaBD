ab5ccebd9257fe13674b5255caa95af4f59e1763
amarchena002 <videojuegos.eui-vi@hotmail.com>
2019-02-21 13:07:05 +0100

    table and database

---
+++ MiniSQLEngine/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine
+{
+    public class Database
+    {
+        private string name;
+        List<Table> tables = new List<Table>();
+
+        public Database(string pName)
+        {
+            name = pName;
+
+        }
+
+        public void CreateTable(string name, string pColumns)
+        {
+            Table table = new Table(name, pColumns);
+            tables.Add(table);
+        }
+
+        public void DeleteTable(string name)
+        {
+            for (int i = 0; i < tables.Count; i++)
+            {
+                if (tables[i].getName().Equals(name))
+                {
+                    tables[i].delete(null);
+
+                }
+            }
+
+        }
+        public MiniSQLEngine.MiniSQL Parse(string query)
+        {
+            return null;
+        }
+    }
+}
--- MiniSQLEngine/MiniSQL.cs
+++ MiniSQLEngine/MiniSQL.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Databases;
 namespace MiniSQLEngine
 {
   public class MiniSQL
     {
         public string Execute(Database pDatabase)
-        {
+        {        
             return null;
         }
         
