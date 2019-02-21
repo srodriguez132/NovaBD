11cfd15e6ac7805e014986ae775d7564253d2eba
srodriguez132 <videojuegos.eui-vi@hotmail.com>
2019-02-21 10:54:17 +0100

    database

--- Database/Database.cs
+++ Database/Database.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace Database
 {
-    public class Database
+    class Database
     {
+        private string name;
+        List<Table> tables = new List<Table>();
+
+        public Database(string pName)
+        {
+            name = pName;
+
+        }
+
+        public void CreateTable(string name, string pColumns)
+        {
+            Table table = new Table(name, pColumns);
+            tables.Add(table);
+        }
+
+        public void DeleteTable(string name)
+        {
+
+        }
+        //parse
     }
 }
