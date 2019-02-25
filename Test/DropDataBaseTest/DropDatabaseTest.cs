6e6aee49ba57c49164a5a49901227451385bae39
amarchena002 <videojuegos.eui-vi@hotmail.com>
2019-02-25 12:26:35 +0100

    test and insert

---
+++ Test/DropDataBaseTest/DropDatabaseTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniSQLEngine;
+namespace DropDataBaseTest
+{
+    [TestClass]
+    public class DropDataBaseTest
+    {
+        [TestMethod]
+        public void DropDatabaseTest()
+        {
+            Match result;
+            DropDataBase ddb = new DropDataBase();
+            result = ddb.DropDatabase("DROP DATABASE database;");
+            Assert.AreEqual("database", result.Groups[1].Value);
+        }
+    }
+}
