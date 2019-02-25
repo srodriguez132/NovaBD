2305ebf81f3362470394e367a3f5537b7700b480
andoni10 <videojuegos.eui-vi@hotmail.com>
2019-02-25 11:55:05 +0100

    regularExpressions class

--- MiniSQLEngine/CreateTable.cs
+++ MiniSQLEngine/CreateTable.cs
@@ -19,18 +19,18 @@ namespace MiniSQLEngine
             string regExp = @"CREATE\s+TABLE\s+(\w+)\s+\(([^()]*)\);";
             //string input = "CREATE TABLE tabla (name string, edad int)";
             Match match = Regex.Match(query, regExp);
-            if (match.Success)
-            {
-                string fileName = match.Groups[1].Value;
-                string attributes = match.Groups[2].Value;
-                string path = @"C:\Users\docencia\Desktop\";
-                using (StreamWriter writer = File.CreateText(path + fileName + ".txt"))
-                {
-                    writer.WriteLine(fileName + ";");
-                    writer.Write(attributes + ";");
+           // if (match.Success)
+           // {
+             //   string fileName = match.Groups[1].Value;
+             //   string attributes = match.Groups[2].Value;
+              //  string path = @"C:\Users\docencia\Desktop\";
+              //  using (StreamWriter writer = File.CreateText(path + fileName + ".txt"))
+              //  {
+              //      writer.WriteLine(fileName + ";");
+              //      writer.Write(attributes + ";");
 
-                }
-            }
+              //  }
+          //  }
 
             name = (string)match.Groups[1].Value;
             atributes = (string)match.Groups[2].Value;
---
+++ MiniSQLEngine/RegularExpressions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine
+{
+    class RegularExpressions
+    {
+        public const string BackupDatabase = @"BACKUP DATABASE\s+(\w+)\s+TO DISK = ('\w+');";
+        public const string CreateDataBase = @"CREATE\s+DATABASE\s+(\w+);";
+        public const string CreateTable = @"CREATE\s+TABLE\s+(\w+)\s+\(([^()]*)\);";
+        public const string Delete = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+)\s+=\s+(\w+);";
+        public const string DropDataBase = @"DROP\s+DATABASE\s+(\w+);";
+        public const string DropTable = @"DROP\s+TABLE\s+(\w+);";
+        public const string Insert = @"INSERT\s+INTO\s+(\w+)\s+\(([\w=,]+)\)\s+VALUES\s+\(([\w,]+)\);";
+        public const string Select = @"SELECT\s+(\w+|\*)(\s+|\,\s+(\w+)\s+)FROM\s+(\w+)(\s+WHERE\s+(\w+)\s+=\s+(\w+);|;)";
+        public const string Update = @"UPDATE\s+(\w+)\s+SET\s+([\w=,]+)\s+WHERE\s+(\w+);";
+    }
+}
