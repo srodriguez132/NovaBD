1315e4ae8edd42e47f1f0229957033bd7673f17a
srodriguez132 <videojuegos.eui-vi@hotmail.com>
2019-02-25 12:07:41 +0100

    update match deleted

--- MiniSQLEngine/Update.cs
+++ MiniSQLEngine/Update.cs
@@ -18,16 +18,7 @@ namespace MiniSQLEngine
             this.updateInfo = updateInfo;
             this.condition = condition;
         }
-        public Match update(String query)
-        {
-            //UPDATE table SET column1=value1, column2=value2 WHERE condition;
-            //UPDATE table SET column=value WHERE condition;
-
-            string regexp = @"UPDATE\s+(\w+)\s+SET\s+([\w=,]+)\s+WHERE\s+(\w+);";
-            Match match = Regex.Match(query, regexp);
-            return match;
-        }
-
+      
         public string Execute(Database pDatabase)
         {
             Table pTable = pDatabase.GetTable(tableName);
