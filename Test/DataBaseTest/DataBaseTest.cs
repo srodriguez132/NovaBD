4c3121d09359c83b981567aef7ef942c7d423745
amarchena002 <videojuegos.eui-vi@hotmail.com>
2019-02-27 10:17:01 +0100

    test

--- Test/DataBaseTest/DataBaseTest.cs
+++ Test/DataBaseTest/DataBaseTest.cs
@@ -7,12 +7,39 @@ namespace DataBaseTest
     public class DataBaseTest
     {
         [TestMethod]
-        public void parseTest()
+        public void parseDeleteTest()
         {
             Database db = new Database("name");
             MiniSQLEngine.MiniSQL q1 = db.Parse("DELETE FROM table WHERE id = id1;");
-            Assert.IsInstanceOfType(q1, typeof(Delete));
-           
+            Assert.IsInstanceOfType(q1, typeof(Delete));          
+        }
+        [TestMethod]
+        public void parseCreateDatabaseTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("CREATE DATABASE database");
+            Assert.IsInstanceOfType(q1, typeof(CreateDataBase));
+        }
+        [TestMethod]
+        public void parseCreateTableTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("CREATE TABLE table");
+            Assert.IsInstanceOfType(q1, typeof(CreateTable));
+        }
+        [TestMethod]
+        public void parseDropTableTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("DROP TABLE table");
+            Assert.IsInstanceOfType(q1, typeof(DropTable));
+        }
+        [TestMethod]
+        public void parseInsertTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("INSERT INTO table VALUES ()");
+            Assert.IsInstanceOfType(q1, typeof(Insert));
         }
     }
 }
