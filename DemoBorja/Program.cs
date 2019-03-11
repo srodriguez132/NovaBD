0b0b7c66b315c63d7a206a56fba355832bc88476
andoni10 <videojuegos.eui-vi@hotmail.com>
2019-03-11 10:49:55 +0100

    FileName Window

--- DemoBorja/Program.cs
+++ DemoBorja/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MiniSQLEngine;
 
@@ -17,40 +18,50 @@ namespace DemoBorja
             string path = @".\";
             Console.WriteLine("Write the name of the file you want to read ");
             string inputfile = Console.ReadLine();
-            using (StreamWriter writer = File.CreateText(path + "output.txt"))
+            try
             {
-                int c = 1;
-                Stopwatch stopWatch = new Stopwatch();
-                Database db = new Database("database1");
-                writer.WriteLine("# TEST " + c);
-                string[] lines = System.IO.File.ReadAllLines(@".\bin\TesterInput-example.txt");
-                for (int i = 0; i < lines.Length; i++)
+                using (StreamWriter writer = File.CreateText(path + "output.txt"))
                 {
-                    stopWatch.Start();
-                    if (lines[i] == "")
-                    {
-                        stopWatch.Stop();
-                        writer.WriteLine("TOTAL TIME: " + Convert.ToDecimal(stopWatch.Elapsed.TotalSeconds)+"s");
-                        stopWatch = new Stopwatch();
-                        stopWatch.Start();
-                        c++;
-                        db = new Database("database"+i);
-                        writer.WriteLine("");
-                        writer.WriteLine("# TEST " + c);
-                    }
-                    else
-                    {
-                        Stopwatch stopWatch1 = new Stopwatch();
-                        stopWatch1.Start();
-                        string res = db.Query(lines[i]);
-                        stopWatch1.Stop();
-                        writer.WriteLine( res + " (" + Convert.ToDecimal(stopWatch1.Elapsed.TotalSeconds) + "s)");
-                    }
-                }
-                stopWatch.Stop();
-                writer.WriteLine("TOTAL TIME: " + Convert.ToDecimal(stopWatch.Elapsed.TotalSeconds) + "s");
-            }
+                    int c = 1;
+                    Stopwatch stopWatch = new Stopwatch();
+                    Database db = new Database("database1");
+                    writer.WriteLine("# TEST " + c);
 
+                    string[] lines = System.IO.File.ReadAllLines(@".\Inputs\" + inputfile + ".txt");
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        stopWatch.Start();
+                        if (lines[i] == "")
+                        {
+                            stopWatch.Stop();
+                            writer.WriteLine("TOTAL TIME: " + Convert.ToDecimal(stopWatch.Elapsed.TotalSeconds) + "s");
+                            stopWatch = new Stopwatch();
+                            stopWatch.Start();
+                            c++;
+                            db = new Database("database" + i);
+                            writer.WriteLine("");
+                            writer.WriteLine("# TEST " + c);
+                        }
+                        else
+                        {
+                            Stopwatch stopWatch1 = new Stopwatch();
+                            stopWatch1.Start();
+                            string res = db.Query(lines[i]);
+                            stopWatch1.Stop();
+                            writer.WriteLine(res + " (" + Convert.ToDecimal(stopWatch1.Elapsed.TotalSeconds) + "s)");
+                        }
+                    }
+                    stopWatch.Stop();
+                    writer.WriteLine("TOTAL TIME: " + Convert.ToDecimal(stopWatch.Elapsed.TotalSeconds) + "s");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file doesn´t exist ");
+                //The program stops for 5 seconds
+                Thread.Sleep(5000);
+
+            }
         }
     }
 }
