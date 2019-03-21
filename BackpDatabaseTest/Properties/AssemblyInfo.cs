d26e49f7030ee6e72ca1c579eb7b95af391d877b
juneanton <videojuegos.eui-vi@hotmail.com>
2019-03-21 12:53:19 +0100

    version

--- BackpDatabaseTest/Properties/AssemblyInfo.cs
+++ BackpDatabaseTest/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("e4769200-a9ad-45a5-be9b-17985e8715e0")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- UpdateTest/Properties/AssemblyInfo.cs
+++ UpdateTest/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("27e1d4af-52ba-49ce-b0f9-7069586d872d")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
