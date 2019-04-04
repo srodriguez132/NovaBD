7335f844508b0075dfa9b1add0b3d1dc6b1bd4da
juneanton <videojuegos.eui-vi@hotmail.com>
2019-04-04 11:57:22 +0200

    security tests

--- Test/DataBaseTest/DataBaseTest.cs
+++ Test/DataBaseTest/DataBaseTest.cs
@@ -69,5 +69,49 @@ namespace DataBaseTest
             MiniSQLEngine.MiniSQL q1 = db.Parse("SELECT column FROM table;");
             Assert.IsInstanceOfType(q1, typeof(Select));
         }
+
+        //Security tests
+        [TestMethod]
+        public void addUserTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("ADD USER(user, password, security_profile);");
+            Assert.IsInstanceOfType(q1, typeof(AddUser));
+        }
+        [TestMethod]
+        public void createSecurityTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("CREATE SECURITY PROFILE security_profile;");
+            Assert.IsInstanceOfType(q1, typeof(CreateSecurity));
+        }
+        [TestMethod]
+        public void dropSecurityTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("DROP SECURITY PROFILE security_profile;");
+            Assert.IsInstanceOfType(q1, typeof(DropSecurity));
+        }
+        [TestMethod]
+        public void grantTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("GRANT privilege_type ON table TO security_profile;");
+            Assert.IsInstanceOfType(q1, typeof(Grant));
+        }
+        [TestMethod]
+        public void revokeTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("REVOKE privilege_type ON table TO security_profile;");
+            Assert.IsInstanceOfType(q1, typeof(Revoke));
+        }
+        [TestMethod]
+        public void deleteUserTest()
+        {
+            Database db = new Database("name");
+            MiniSQLEngine.MiniSQL q1 = db.Parse("DELETE USER user;");
+            Assert.IsInstanceOfType(q1, typeof(DeleteUser));
+        }
     }
 }
