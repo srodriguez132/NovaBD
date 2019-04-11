df612bde677da5142f2ec69264022ccc372f36b0
srodriguez132 <videojuegos.eui-vi@hotmail.com>
2019-04-11 08:42:17 +0200

    fixed console class error

---
+++ MiniSQLDBConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿using MiniSQLEngine;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MiniSQLDBConsole
+{
+    class Program
+    {
+        private static Database db;
+        private static User user;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write the name of the database: ");
+            string input = Console.ReadLine();
+            db = new Database(input);
+            Console.WriteLine("Write the name of the user: ");
+            string inputUser = Console.ReadLine();
+            user = db.GetUser(inputUser);
+            while (user == null)
+            {
+                Console.WriteLine("Security user does not exist");
+                Console.WriteLine("Write the name of the user: ");
+                inputUser = Console.ReadLine();
+                user = db.GetUser(inputUser);
+            }
+            string inputPass = null;
+            while (inputPass == null || user.GetPassword() != inputPass)
+            {
+
+                Console.WriteLine("Write the password of the user: ");
+                inputPass = Console.ReadLine();
+                if (user.GetName() == Constants.adminName && user.GetPassword() == inputPass)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Correct password");
+            db.setCurrentUser(user);
+
+
+            try
+            {
+                Console.WriteLine("Write the sentences: ");
+                input = Console.ReadLine();
+                Stopwatch stopWatch = new Stopwatch();
+
+                while (!input.Equals("end"))
+                {
+                    stopWatch.Start();
+                    string res = db.Query(input);
+                    stopWatch.Stop();
+                    Console.WriteLine(res + " (" + Convert.ToDecimal(stopWatch.Elapsed.TotalSeconds) + "s)");
+                    Console.WriteLine("Write the sentences: ");
+                    input = Console.ReadLine();
+
+                }
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+        }
+
+    }
+}
+
